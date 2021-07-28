e91c0821ded079b738e7debec8b06caed0bb44f4
Developer Izi <hyunseong3020@naver.com>
2021-07-28 12:25:58 +0900

    Try button click1(FAILED)

--- venv1/djtest/dj/views.py
+++ venv1/djtest/dj/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'dj/index.html')
+
+
+def request_page(request):
+    if(request.GET.get('mybtn')):
+        (int(request.GET.get('mytextbox')))
+    
+    return render(request,'dj/index.html')
