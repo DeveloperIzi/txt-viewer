dee6cdba7a6b0f0d0a68d5ae124239abb62e3efa
Developer Izi <hyunseong3020@naver.com>
2021-07-28 11:28:37 +0900

    Link index.html to django

--- venv1/djtest/djtest/urls.py
+++ venv1/djtest/djtest/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import dj.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', dj.views.index, name='index'),
 ]
